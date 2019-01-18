ec4ad06b97614eea33e257c682ed478392c4b64c
Paige Williams <paw145@humboldt.edu>
2019-01-18 08:43:01 -0800

    Gather user input from form.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function(){
+  $(".doctor-finder").submit(function(event){
+    event.preventDefault();
+
+    const first = $("#doctor-first-name").val();
+    const last = $("#doctor-last-name").val();
+    const keyword = $("#keyword").val();
+
+    
+
+  });
+});
