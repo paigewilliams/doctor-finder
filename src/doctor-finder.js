a3250371669000ca0473ce6a9a8ff7696503795c
Paige Williams <paw145@humboldt.edu>
2019-01-18 09:48:51 -0800

    Add second promise for differnt search criteria.

---
+++ src/doctor-finder.js
@@ -0,0 +1,32 @@
+export class DoctorFinder {
+  findDoctorByName(firstName, lastName) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstNAme}&last_name=${lastName}&location=or-portland&skip=0&limit=10&user_ke=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    })
+  }
+  findDoctorByKeyword(keyword) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=Broken%20bone&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    })
+  }
+}
