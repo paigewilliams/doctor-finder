7042103a373df1ecfb456dbf335c345e4bf06a54
Paige Williams <paw145@humboldt.edu>
2019-01-18 10:03:26 -0800

    Successful API call with user input first and last name.

--- src/doctor-finder.js
+++ src/doctor-finder.js
@@ -2,7 +2,7 @@ export class DoctorFinder {
   findDoctorByName(firstName, lastName) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstNAme}&last_name=${lastName}&location=or-portland&skip=0&limit=10&user_ke=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
+import { DoctorFinder } from './../src/doctor-finder'
+
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function(){
-  $(".doctor-finder").submit(function(event){
-    event.preventDefault();
 
+  const finder = new DoctorFinder();
+
+  $(".doctor-finder-name").submit(function(event){
+    event.preventDefault();
     const first = $("#doctor-first-name").val();
     const last = $("#doctor-last-name").val();
+    let promise = finder.findDoctorByName(first, last);
+
+    promise.then(function(response){
+      let body = JSON.parse(response);
+      
+    })
+  });
+
+  $(".doctor-finder-keyword").submit(function(event){
+    event.preventDefault();
     const keyword = $("#keyword").val();
-
-    
-
+    console.log(keyword)
   });
 });
