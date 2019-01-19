36a5a42cf64087575298145fabbf152911b3a9a4
Paige Williams <paw145@humboldt.edu>
2019-01-18 16:19:24 -0800

    Empty output div and fix indentation.

--- src/main.js
+++ src/main.js
@@ -6,79 +6,81 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
 
- function showDoctorInfo(doctors){
-   for(let i = 0; i < doctors.length; i++) {
-     $(".output").append(
-       `<div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title"> ${doctors[i].firstName} ${doctors[i].lastName}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${doctors[i].spec}</h6
-          <h6 class="card-subtitle mb-2 text-muted"> ${doctors[i].street} ${doctors[i].city}, ${doctors[i].state} ${doctors[i].zip}</h6>
-          <p class="card-text">Phone Number: ${doctors[i].phoneNumber}<br><p>Taking new patients? ${doctors[i].newPatient}</p>
-        </div>
+function showDoctorInfo(doctors){
+  for(let i = 0; i < doctors.length; i++) {
+    $(".output").append(
+      `<div class="card" style="width: 18rem;">
+      <div class="card-body">
+      <h5 class="card-title"> ${doctors[i].firstName} ${doctors[i].lastName}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${doctors[i].spec}</h6
+      <h6 class="card-subtitle mb-2 text-muted"> ${doctors[i].street} ${doctors[i].city}, ${doctors[i].state} ${doctors[i].zip}</h6>
+      <p class="card-text">Phone Number: ${doctors[i].phoneNumber}<br><p>Taking new patients? ${doctors[i].newPatient}</p>
+      </div>
       </div>`)
     }
   }
 
- function parseData(response){
-   let body = JSON.parse(response);
-   const data = body.data;
-   const doctorData = new DoctorData();
-   doctorData.createAllDocs(data);
-   const allDocs = doctorData.allDocs;
-   if (allDocs.length === 0){
-     $("#no-doctors").show();
-   } else {
-     $("#no-doctors").hide();
-     showDoctorInfo(allDocs)
-   }
- }
+  function parseData(response){
+    let body = JSON.parse(response);
+    const data = body.data;
+    const doctorData = new DoctorData();
+    doctorData.createAllDocs(data);
+    const allDocs = doctorData.allDocs;
+    if (allDocs.length === 0){
+      $("#no-doctors").show();
+    } else {
+      $("#no-doctors").hide();
+      showDoctorInfo(allDocs)
+    }
+  }
 
- function parseSymptomData(response){
-   let body = JSON.parse(response);
-   const allSymptoms = body.data;
-   const allSymptomsArray = [];
-   for(let i = 0; i < allSymptoms.length; i++){
-     allSymptomsArray.push(`<option value="${allSymptoms[i].name}">${allSymptoms[i].name}</option>`);
-   }
-   $("#keyword").html(allSymptomsArray.join(''));
- }
+  function parseSymptomData(response){
+    let body = JSON.parse(response);
+    const allSymptoms = body.data;
+    const allSymptomsArray = [];
+    for(let i = 0; i < allSymptoms.length; i++){
+      allSymptomsArray.push(`<option value="${allSymptoms[i].name}">${allSymptoms[i].name}</option>`);
+    }
+    $("#keyword").html(allSymptomsArray.join(''));
+  }
 
- function errorMessage(error){
-   $("#error").show();
-   $(".error").html(`There was error processing your query: ${error.message}`)
- }
+  function errorMessage(error){
+    $("#error").show();
+    $(".error").html(`There was error processing your query: ${error.message}`)
+  }
 
-$(document).ready(function(){
-  const symptoms = new SymptomFinder()
-  let promise = symptoms.findSymptom()
-  promise.then(function(response){
-    parseSymptomData(response)
-    const finder = new DoctorFinder();
+  $(document).ready(function(){
+    const symptoms = new SymptomFinder()
+    let promise = symptoms.findSymptom()
+    promise.then(function(response){
+      parseSymptomData(response)
+      const finder = new DoctorFinder();
 
-    $(".doctor-finder-name").submit(function(event){
-      event.preventDefault();
-      const first = $("#doctor-first-name").val();
-      const last = $("#doctor-last-name").val();
-      let promise = finder.findDoctorByName(first, last);
-      promise.then(function(response){
-        $(".doctor-finder-name")[0].reset();
-        parseData(response);
-      }, function(error){
-        errorMessage(error)
-      })
-    });
+      $(".doctor-finder-name").submit(function(event){
+        event.preventDefault();
+        const first = $("#doctor-first-name").val();
+        const last = $("#doctor-last-name").val();
+        $(".output").empty();
+        let promise = finder.findDoctorByName(first, last);
+        promise.then(function(response){
+          $(".doctor-finder-name")[0].reset();
+          parseData(response);
+        }, function(error){
+          errorMessage(error)
+        })
+      });
 
-    $(".doctor-finder-keyword").submit(function(event){
-      event.preventDefault();
-      const keyword = $("#keyword").val();
-      let promise = finder.findDoctorByKeyword(keyword);
-      promise.then(function(response){
-        $(".doctor-finder-keyword")[0].reset();
-        parseData(response);
-      }, function(error){
-        errorMessage(error)
-      })
-    });
-  })
-});
+      $(".doctor-finder-keyword").submit(function(event){
+        event.preventDefault();
+        const keyword = $("#keyword").val();
+        $(".output").empty();
+        let promise = finder.findDoctorByKeyword(keyword);
+        promise.then(function(response){
+          $(".doctor-finder-keyword")[0].reset();
+          parseData(response);
+        }, function(error){
+          errorMessage(error)
+        })
+      });
+    })
+  });
